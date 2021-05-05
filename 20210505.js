e32702ba4fb2874450517a673fd6deafb0412903
ipadorusa <ipadorusa@gmail.com>
2021-05-05 23:54:31 +0900

    20210505 추가 공부

--- 20210505.js
+++ 20210505.js
@@ -45,3 +45,26 @@ function solution(s) {
 
 let str = 'KKHSSSSSSSE';
 solution(str);
+
+function solution(n, arr) {
+  let answer,
+    max = Number.MIN_SAFE_INTEGER;
+  for (let x of arr) {
+    let sum = x
+      .toString()
+      .split('')
+      .reduce((acc, cur) => {
+        return acc + Number(cur);
+      }, 0);
+    if (sum > max) {
+      max = sum;
+      answer = x;
+    } else if (sum === max) {
+      if (x > answer) answer = x;
+    }
+  }
+  return answer;
+}
+
+let arr = [128, 460, 603, 40, 521, 137, 123];
+solution(7, arr);
