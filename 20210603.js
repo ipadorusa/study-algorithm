1b8a82dc5db973a82ad8d19b2fd268725d6feabe
Kimwoosuk <ipadorusa@gmail.com>
2021-06-03 13:03:31 +0900

    Create 20210603.js

---
+++ 20210603.js
@@ -0,0 +1,21 @@
+const arr = [1,3,1,2,3]
+const limit = 5;
+
+function sol(limit, arr) {
+  let answer = 0;
+  let sum = 0;
+  let lt = 0;
+
+  for(let rt=0;rt<arr.length;rt++) {
+    sum+= arr[rt];
+    while(sum>limit) {
+      sum-=arr[lt++];
+    }
+    answer += (rt-lt+1)
+
+  }
+
+
+  return answer;
+}
+sol(5,arr);
