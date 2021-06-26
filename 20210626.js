c0b0d730a5c742ef05c3e727559edbee881de301
ipadorusa <ipadorusa@gmail.com>
2021-06-26 22:36:48 +0900

    20210626

--- 20210626.js
+++ 20210626.js
@@ -1,5 +1,12 @@
 function solution(s){
-  let answer = [];
+  let answer = "YES";
+  let stack = [];
+  for(let x of s) {
+    if(x === '(') stack.push(x);
+    else if(x === ')') stack.pop();
+  }
+  stack.length > 0 ? answer = "no" : answer
+  return answer;
 }
 
 let a="(()(()))(()";
